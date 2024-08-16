2962a375e3ddf57b8f6da4dd160cc0e441b7e9b1
yvan.noubom@pulse-team.de <yvan.noubom@pulse-team.de>
2024-08-16 12:48:48 +0200

    Benefits Body creaeted

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import HText from "@/shared/HText";
+import { HomeModernIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
+import { UserGroupIcon } from "@heroicons/react/24/solid";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilities",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
@@ -29,6 +60,24 @@ const Benefits = ({ setSelectedPage }: Props) => {
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
           </p>
+        </motion.div>
+        {/* BENEFITS */}
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
         </motion.div>
       </motion.div>
     </section>
